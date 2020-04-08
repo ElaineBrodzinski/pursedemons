d5ce48e9cf26a563afd793558c879b52ff169678
Jeremy Banks <_@jeremy.ca>
2020-04-08 00:43:05 -0400

    Boo also follows the mouse, and lights go on and off.

--- game.py
+++ game.py
@@ -6,24 +6,25 @@ pygame.init()
 
 # Initialize a clock we'll use to set the framerate.
 clock = pygame.time.Clock()
-fps = 30
+fps = 60
 
 # Set up the window we'll be drawing in
-window = pygame.display.set_mode([512, 512])
+window = pygame.display.set_mode([768, 512])
+pygame.display.set_caption("Boo!")
 
 # We'll run until the user asks to quit.
 running = True
 
 # We adjust these variables to control the Boo we draw below.
-x_position = 100.0
-y_position = 200.0
+x_position = 128.0
+y_position = 256.0
 
 # The amount to change the x and y positions of the Boo per second.
 x_speed = 0.0
 y_speed = 0.0
 
 # Load the image of Boo we'll be displaying.
-boo_image = pygame.image.load("boo.png").convert_alpha()
+boo_image = pygame.image.load("assets/boo.png").convert_alpha()
 
 # Loop forever until we set running to False, when the user tries to exit.
 while running:
@@ -32,36 +33,45 @@ while running:
 
     # Loop over every input or event that has happened since the last frame.
     for event in pygame.event.get():
-        # If the user pressed the close button or alt+F4 or whatever, then...
-        if event.type == pygame.QUIT:
-            print("Exiting because the user told us to")
-            running = False
+        print(event)
 
-        # ...otherwise, log a message about the unrecognized event in case it's useful later.
-        else:
-            print("An event happened that I don't recognize:", event)
+        if event.type == pygame.QUIT or (
+            event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+        ):
+            print("Exiting because the user told us to.")
+            running = False
+            break  # immediately jump out of loop
+
+        if event.type == pygame.MOUSEMOTION:
+            # if you move mouse over the window, center Boo on it
+            x_position = event.pos[0] - boo_image.get_width() / 2
+            y_position = event.pos[1] - boo_image.get_height() / 2
 
     keys = pygame.key.get_pressed()
-    x_speed = 0
-    y_speed = 0
-    if keys[pygame.K_LEFT]:
-        x_speed -= 50
-    if keys[pygame.K_RIGHT]:
-        x_speed += 50
-    if keys[pygame.K_UP]:
-        y_speed -= 50
-    if keys[pygame.K_DOWN]:
-        y_speed += 50
-    # note that this is "if" instead of "elif", because multiple can happen at the same time,
-    # one doesn't exclude the following ones, like it did when we were looking at a single key
-    # event at the same time.
+    x_speed = 0.0
+    y_speed = 0.0
+
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        x_speed -= 256
+
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        x_speed += 256
+
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
+        y_speed -= 256
+
+    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        y_speed += 256
 
     # Update position using the speed (per second), divided by the number of frames per second.
     x_position += x_speed / fps
     y_position += y_speed / fps
 
     # Fill the background with white, overwriting the previous frame.
-    window.fill((255, 255, 255))
+    if pygame.mouse.get_focused():
+        window.fill((255, 255, 255))
+    else:
+        window.fill((200, 200, 200))
 
     # Draw a Boo with the current position, color, and size.
     position = int(round(x_position)), int(round(y_position))
